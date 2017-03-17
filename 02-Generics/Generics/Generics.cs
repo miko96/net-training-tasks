cd21e81f7fa2efc79af5c0d0fc491ad621ea36c0
Alexandr Matveychuk <eco_ideas@inbox.ru>
2017-03-17 16:48:33 +0300

    Generics tasks are solved

--- 02-Generics/Generics/Generics.cs
+++ 02-Generics/Generics/Generics.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
 
 namespace Task.Generics {
 
@@ -23,8 +28,7 @@ namespace Task.Generics {
 		///   { new TimeSpan(1, 0, 0), new TimeSpan(0, 0, 30) } => "01:00:00,00:00:30",
 		/// </example>
 		public static string ConvertToString<T>(this IEnumerable<T> list) {
-            // TODO : Implement ConvertToString<T>      
-			throw new NotImplementedException();
+            return string.Join(ListSeparator.ToString(), list);
 		}
 
 		/// <summary>
@@ -44,9 +48,10 @@ namespace Task.Generics {
 		///  "1:00:00,0:00:30" for TimeSpan =>  { new TimeSpan(1, 0, 0), new TimeSpan(0, 0, 30) },
 		///  </example>
 		public static IEnumerable<T> ConvertToList<T>(this string list) {
-			// TODO : Implement ConvertToList<T>
-			// HINT : Use TypeConverter.ConvertFromString method to parse string value
-			throw new NotImplementedException();
+            var values = list.Split(ListSeparator);
+            TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
+            foreach (var value in values)
+                yield return (T)converter.ConvertFrom(value);
 		}
 
 	}
@@ -61,114 +66,171 @@ namespace Task.Generics {
 		/// <param name="index1">first index</param>
 		/// <param name="index2">second index</param>
 		public static void SwapArrayElements<T>(this T[] array, int index1, int index2) {
-			// TODO : Implement SwapArrayElements<T>
-			throw new NotImplementedException();
+            T temp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp;
 		}
 
-		/// <summary>
-		///   Sorts the tuple array by specified column in ascending or descending order
-		/// </summary>
-		/// <param name="array">source array</param>
-		/// <param name="sortedColumn">index of column</param>
-		/// <param name="ascending">true if ascending order required; otherwise false</param>
-		/// <example>
-		///   source array : 
-		///   { 
-		///     { 1, "a", false },
-		///     { 3, "b", false },
-		///     { 2, "c", true  }
-		///   }
-		///   result of SortTupleArray(array, 0, true) is sort rows by first column in a ascending order: 
-		///   { 
-		///     { 1, "a", false },
-		///     { 2, "c", true  },
-		///     { 3, "b", false }
-		///   }
-		///   result of SortTupleArray(array, 1, false) is sort rows by second column in a descending order: 
-		///   {
-		///     { 2, "c", true  },
-		///     { 3, "b", false }
-		///     { 1, "a", false },
-		///   }
-		/// </example>
-		public static void SortTupleArray<T1, T2, T3>(this Tuple<T1, T2, T3>[] array, int sortedColumn, bool ascending) {
-			// TODO :SortTupleArray<T1, T2, T3>
-			// HINT : Add required constraints to generic types
-		}
+        /// <summary>
+        ///   Sorts the tuple array by specified column in ascending or descending order
+        /// </summary>
+        /// <param name="array">source array</param>
+        /// <param name="sortedColumn">index of column</param>
+        /// <param name="ascending">true if ascending order required; otherwise false</param>
+        /// <example>
+        ///   source array : 
+        ///   { 
+        ///     { 1, "a", false },
+        ///     { 3, "b", false },
+        ///     { 2, "c", true  }
+        ///   }
+        ///   result of SortTupleArray(array, 0, true) is sort rows by first column in a ascending order: 
+        ///   { 
+        ///     { 1, "a", false },
+        ///     { 2, "c", true  },
+        ///     { 3, "b", false }
+        ///   }
+        ///   result of SortTupleArray(array, 1, false) is sort rows by second column in a descending order: 
+        ///   {
+        ///     { 2, "c", true  },
+        ///     { 3, "b", false }
+        ///     { 1, "a", false },
+        ///   }
+        /// </example>
+        public static void SortTupleArray<T1, T2, T3>(this Tuple<T1, T2, T3>[] array, int sortedColumn, bool ascending)
+        where T1 : IComparable where T2 : IComparable where T3 : IComparable
+        {
+            var asc = Convert.ToInt32(ascending);
 
-	}
+            Comparison<Tuple<T1, T2, T3>>[] comparer =
+            {
+                (x,y)=>x.Item1.CompareTo(y.Item1) * asc,
+                (x,y)=>x.Item2.CompareTo(y.Item2) * asc,
+                (x,y)=>x.Item3.CompareTo(y.Item3) * asc
+            };
 
-	/// <summary>
-	///   Generic singleton class
-	/// </summary>
-	/// <example>
-	///   This code should return the same MyService object every time:
-	///   MyService singleton = Singleton<MyService>.Instance;
-	/// </example>
-	public static class Singleton<T> {
-		// TODO : Implement generic singleton class 
+            try
+            {
+                Array.Sort(array, comparer[sortedColumn]);
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                throw ex;
+            }
+        }
 
-		public static T Instance {
-			get { throw new NotImplementedException(); }
-		}
-	}
+    }
+
+    /// <summary>
+    ///   Generic singleton class
+    /// </summary>
+    /// <example>
+    ///   This code should return the same MyService object every time:
+    ///   MyService singleton = Singleton<MyService>.Instance;
+    /// </example>
+    public static class Singleton<T>
+    {
+        // TODO : Implement generic singleton class 
+        private static T instance = default(T);
+
+        private static Object CreateInstance(Type t)
+        {
+            try
+            {
+                return Activator.CreateInstance(t);                
+            }
+            catch (ArgumentException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = (T)CreateInstance(typeof(T));
+                return instance;
+            }
+        }
+    }
 
 
 
 	public static class FunctionExtentions {
-		/// <summary>
-		///   Tries to invoke the specified function up to 3 times if the result is unavailable 
-		/// </summary>
-		/// <param name="function">specified function</param>
-		/// <returns>
-		///   Returns the result of specified function, if WebException occurs duaring request then exception should be logged into trace 
-		///   and the new request should be started (up to 3 times).
-		/// </returns>
-		/// <example>
-		///   Sometimes if network is unstable it is required to try several request to get data:
-		///   
-		///   Func<string> f1 = ()=>(new System.Net.WebClient()).DownloadString("http://www.google.com/");
-		///   string data = f1.TimeoutSafeInvoke();
-		///   
-		///   If the first attemp to download data is failed by WebException then exception should be logged to trace log and the second attemp should be started.
-		///   The second attemp has the same workflow.
-		///   If the third attemp fails then this exception should be rethrow to the application.
-		/// </example>
-		public static T TimeoutSafeInvoke<T>(this Func<T> function) {
-			// TODO : Implement TimeoutSafeInvoke<T>
-			throw new NotImplementedException();
-		}
+        /// <summary>
+        ///   Tries to invoke the specified function up to 3 times if the result is unavailable 
+        /// </summary>
+        /// <param name="function">specified function</param>
+        /// <returns>
+        ///   Returns the result of specified function, if WebException occurs duaring request then exception should be logged into trace 
+        ///   and the new request should be started (up to 3 times).
+        /// </returns>
+        /// <example>
+        ///   Sometimes if network is unstable it is required to try several request to get data:
+        ///   
+        ///   Func<string> f1 = ()=>(new System.Net.WebClient()).DownloadString("http://www.google.com/");
+        ///   string data = f1.TimeoutSafeInvoke();
+        ///   
+        ///   If the first attemp to download data is failed by WebException then exception should be logged to trace log and the second attemp should be started.
+        ///   The second attemp has the same workflow.
+        ///   If the third attemp fails then this exception should be rethrow to the application.
+        /// </example>
+        public static T TimeoutSafeInvoke<T>(this Func<T> function)
+        {
+            int tries = 3;
+            while (true)
+            {
+                try
+                {
+                    return function();
+                }
+                catch (WebException ex)
+                {
+                    Trace.TraceError(ex.ToString());
+                    if (--tries == 0) throw ex;
+                }
+            }
+        }
 
 
-		/// <summary>
-		///   Combines several predicates using logical AND operator 
-		/// </summary>
-		/// <param name="predicates">array of predicates</param>
-		/// <returns>
-		///   Returns a new predicate that combine the specified predicated using AND operator
-		/// </returns>
-		/// <example>
-		///   var result = CombinePredicates(new Predicate<string>[] {
-		///            x=> !string.IsNullOrEmpty(x),
-		///            x=> x.StartsWith("START"),
-		///            x=> x.EndsWith("END"),
-		///            x=> x.Contains("#")
-		///        })
-		///   should return the predicate that identical to 
-		///   x=> (!string.IsNullOrEmpty(x)) && x.StartsWith("START") && x.EndsWith("END") && x.Contains("#")
-		///
-		///   The following example should create predicate that returns true if int value between -10 and 10:
-		///   var result = CombinePredicates(new Predicate<int>[] {
-		///            x=> x>-10,
-		///            x=> x<10
-		///       })
-		/// </example>
-		public static Predicate<T> CombinePredicates<T>(Predicate<T>[] predicates) {
-			// TODO : Implement CombinePredicates<T>
-			throw new NotImplementedException();
-		}
-
-	}
+        /// <summary>
+        ///   Combines several predicates using logical AND operator 
+        /// </summary>
+        /// <param name="predicates">array of predicates</param>
+        /// <returns>
+        ///   Returns a new predicate that combine the specified predicated using AND operator
+        /// </returns>
+        /// <example>
+        ///   var result = CombinePredicates(new Predicate<string>[] {
+        ///            x=> !string.IsNullOrEmpty(x),
+        ///            x=> x.StartsWith("START"),
+        ///            x=> x.EndsWith("END"),
+        ///            x=> x.Contains("#")
+        ///        })
+        ///   should return the predicate that identical to 
+        ///   x=> (!string.IsNullOrEmpty(x)) && x.StartsWith("START") && x.EndsWith("END") && x.Contains("#")
+        ///
+        ///   The following example should create predicate that returns true if int value between -10 and 10:
+        ///   var result = CombinePredicates(new Predicate<int>[] {
+        ///            x=> x>-10,
+        ///            x=> x<10
+        ///       })
+        /// </example>
+        public static Predicate<T> CombinePredicates<T>(Predicate<T>[] predicates)
+        {
+            return (T item) =>
+           {
+               foreach (var predicate in predicates)
+               {
+                   if (!predicate(item))
+                       return false;
+               }
+               return true;
+           };
+        }
+    }
 
 
 }
